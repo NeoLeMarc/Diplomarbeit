a01c79e46fb46b7d2549d6edebc0680750451ca2
Marcel Noe <marcel@marcel-noe.de>
2010-01-10 14:59:06 +0000

    Multithreaded Variante mit Reader und Writer Threads. Events und Queues sind noch nicht implementiert.
    git-svn-id: https://vcs.services.xcore.net/svn/diplomarbeit@3 f4039180-061f-4fdc-bef1-9dcd6f1a9a02

--- connector/MARVConnector.java
+++ connector/MARVConnector.java
@@ -1,5 +1,53 @@
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.*;
+import java.util.*;
+
+class MARVPrioritized implements Comparable {
+    protected int priority;
+
+    public int compareTo(Object o){
+        return 0;
+    }
+
+    public int compareTo(MARVPrioritized o){
+        return this.priority - o.priority;
+    }
+
+}
+
+class MARVCommand extends MARVPrioritized {
+    protected String command;
+    protected boolean result;
+    protected int id = 0;
+    static int maxId;
+
+    MARVCommand(String command, int priority){
+        this.command  = command;
+        this.priority = priority;
+        getUniqueId();
+    }
+
+    synchronized private int getUniqueId(){
+        if(this.id == 0){
+            this.id = ++this.maxId;
+        } 
+        return this.id;
+    }
+
+    public void setResult(boolean result){
+        this.result = result;
+    }
+
+}
+
+class MARVEvent extends MARVPrioritized {
+    protected String raw;
+    
+    MARVEvent(String raw){
+        this.raw = raw;
+    }
+}
 
 class ZigBit {
     int panID;
@@ -42,7 +90,7 @@ class ZigBit {
             return false;
     }
 }
-
+/*
 class SocketAdapter {
     private BufferedReader serialIn;
     private PrintWriter    serialOut;
@@ -71,7 +119,99 @@ class SocketAdapter {
         this.socket.close();
     }
 }
+*/
 
+class SocketReader extends Thread{
+    private Queue<MARVEvent> eventQueue;
+    private boolean            lastStatus;
+    private Semaphore          statusSemaphore = new Semaphore(1, true);
+    private BufferedReader     serialIn; 
+
+    SocketReader(BufferedReader serialIn, Queue<MARVEvent> eventQueue){
+        this.serialIn   = serialIn;
+        this.eventQueue = eventQueue;
+    }
+
+    @Override public void run(){
+        System.out.println("Starting SocketReader with BufferedReader: " + this.serialIn);
+        String line;
+
+        try {
+            while((line = this.serialIn.readLine()) != null){
+                System.out.println("SocketReader: " + line);
+            }
+        } catch (IOException e) {
+            System.err.println("IOException in SocketReader");
+            System.exit(1);
+        }
+    }
+
+}
+
+class SocketWriter extends Thread{
+    private Queue<MARVCommand> commandQueue;
+    private Queue<MARVCommand> resultQueue;
+    private PrintWriter        serialOut;
+
+    SocketWriter(PrintWriter serialOut, Queue<MARVCommand> commandQueue, Queue<MARVCommand> resultQueue){
+        this.serialOut    = serialOut;
+        this.commandQueue = commandQueue;
+        this.resultQueue  = resultQueue;
+    }
+
+    private void writeLine(String line) throws IOException{
+        this.serialOut.print(line + "\r\n");
+        this.serialOut.flush();
+    }
+
+    @Override public void run(){
+        System.out.println("Starting SocketWriter with PrintWriter: " + this.serialOut);
+
+        try {
+            // Initialize ZigBee adapter
+            this.writeLine("AT+WAUTONET=1 +WWAIT=100 Z");
+            this.writeLine("ATS30=1");
+        } catch (IOException e) {
+            System.err.println("IOException in SocketReader");
+            System.exit(1);
+        }
+
+    }
+}
+
+public class MARVConnector {
+    public static void main(String[] args) throws IOException{
+
+        // Create Queues
+        Queue<MARVCommand> commandQueue = new PriorityBlockingQueue<MARVCommand>();
+        Queue<MARVEvent>   eventQueue   = new PriorityBlockingQueue<MARVEvent>();
+        Queue<MARVCommand> resultQueue  = new PriorityBlockingQueue<MARVCommand>();
+
+        // Create Socket Adapter & Connect
+        Socket serialSocket = null;
+        String input;
+       
+        try {
+            serialSocket = new Socket("localhost", 4711);
+
+        } catch (IOException e){
+            System.err.println("Error while creating socket: I/O-Error!");
+            System.exit(1);
+        }
+
+        // Create Reader
+        SocketReader socketReader = new SocketReader(new BufferedReader(new InputStreamReader(serialSocket.getInputStream())), eventQueue);
+
+        // Create Writer
+        SocketWriter socketWriter = new SocketWriter(new PrintWriter(serialSocket.getOutputStream(), true), commandQueue, resultQueue);
+
+        // Start Threads
+        socketReader.start();
+        socketWriter.start();
+    }
+}
+
+/*
 public class MARVConnector {
     public static void main(String[] args) throws IOException {
 
@@ -118,3 +258,5 @@ public class MARVConnector {
 
     }
 }
+*/
+
