d6003f880d74fb3932740c6db9b8f11a9b490679
Marcel Noe <marcel@marcel-noe.de>
2010-02-23 02:34:52 +0000

    Fehlende Dateien für Crossworks
    git-svn-id: https://vcs.services.xcore.net/svn/diplomarbeit@11 f4039180-061f-4fdc-bef1-9dcd6f1a9a02

--- firmware/THUMB Flash Release/main.d
+++ firmware/THUMB Flash Release/main.d
@@ -1,5 +1,4 @@
-THUMB\ Flash\ Release/main.o:  \
- /home/marcel/Ubuntu\ One/diplomarbeit/firmware/main.c \
+THUMB\ Flash\ Release/main.o: /home/marcel/diplomarbeit/firmware/main.c \
   /opt/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM\ 1.7/include/ctl_api.h \
   /opt/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM\ 1.7/include/libarm.h \
   /opt/Rowley\ Associates\ Limited/CrossWorks\ for\ ARM\ 1.7/include/stdlib.h \
