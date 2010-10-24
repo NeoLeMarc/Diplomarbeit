48d60bf9060be1c67de8eb0d95cae51e7d80d5d8
Marcel Noe <marcel@marcel-noe.de>
2010-10-24 20:46:47 +0000

    Webinterface nun mit hübschem Status update und so
    git-svn-id: https://vcs.services.xcore.net/svn/diplomarbeit@124 f4039180-061f-4fdc-bef1-9dcd6f1a9a02

--- manvweb/htdocs/manvweb.js
+++ manvweb/htdocs/manvweb.js
@@ -1,4 +1,7 @@
 /* MANVWeb Javascript prototype */
+var lastBreathing = {};
+var lastPulse     = {};
+
 convertDate = function(timestamp){
     var Zeit = new Date();
     Zeit.setTime(timestamp);
@@ -12,23 +15,61 @@ retrieveData = function(){
     req.onreadystatechange = function(){
         if(req.readyState == 4 && req.status == 200){
             var jsonData = eval('(' + req.responseText + ')');
+            var pulseChanged = 0;
+            var breathingChanged = 0;
 
             // Rendern der Resultate
-            tableRows  = "<table>";
+            tableRows  = "<table id='nodesTable'>";
             tableRows += " <tr>";
             tableRows += "   <td><b>NodeID</b></td>";
             tableRows += "   <td><b>Alarm Puls</b></td>";
             tableRows += "   <td><b>Alarm Atmung</b></td>";
+            tableRows += "   <td><b>Puls</b></td>";
+            tableRows += "   <td><b>Atmung</b></td>";
             tableRows += "   <td><b>Timestamp</b></td>";
             tableRows += " </tr>";
 
             for(i in jsonData){
                 row = jsonData[i];
 
-                tableRows += " <tr>";
+                if(row['alarm_pulse'] == 'true')
+                    tableRows += " <tr class='alert'>";
+                else
+                    tableRows += " <tr class='ok'>";
+
                 tableRows += "  <td>" + row['nodeID']          + "</td>";
-                tableRows += "  <td>" + row['alarm_pulse']     + "</td>";
-                tableRows += "  <td>" + row['alarm_breathing'] + "</td>";
+                tableRows += "  <td id='alarm'>" + row['alarm_pulse']     + "</td>";
+                tableRows += "  <td id='alarm'>" + row['alarm_breathing'] + "</td>";
+               
+                pulse     = "";
+                breathing = "";
+                nodeID    = row['nodeID'];
+
+                if(row['dataMessages'].length){
+                    pulseValue      = row['dataMessages'][0]['pulse'];
+                    breathingValue  = row['dataMessages'][0]['breathing'];
+
+                    pulse     += " " + pulseValue;
+                    breathing += " " + breathingValue;
+
+                    // Flashing
+                    if(pulseValue != lastPulse[row['nodeID']]){
+                        lastPulse[row['nodeID']] = pulseValue;
+
+                        // Flash blue border
+                        window.setTimeout("flashTable('" + nodeID + "_pulse')", 0);
+                    }
+
+                    if(breathingValue != lastBreathing[row['nodeID']]){
+                        lastBreathing[row['nodeID']] = breathingValue;
+
+                        // Flash blue border
+                        window.setTimeout("flashTable('" + nodeID + "_breathing')", 0);
+                    }
+                }
+
+                tableRows += "    <td id='" + nodeID + "_pulse'>" + pulse + "</td>";
+                tableRows += "    <td id='" + nodeID + "_breathing'>" + breathing + "</td>";
                 tableRows += "  <td>" + convertDate(row['serverTimestamp']) + "</td>";
                 tableRows += " </tr>";
 
@@ -46,8 +87,14 @@ retrieveData = function(){
 
 }
 
+flashTable = function(columnID){
+    column = document.getElementById(columnID);
+    column.className = 'ready';
+}
+
 startUpdating = function(){
     self.retrieveData();
+
 
     // Refetch data after 1 Second
     window.setTimeout("startUpdating()", 1000);
